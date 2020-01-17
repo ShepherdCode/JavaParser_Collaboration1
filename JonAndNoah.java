9388ffc03650e3af3990094a821e8a242f883592
jkatz98 <jkatz01@rams.shepherd.edu>
2020-01-17 13:13:18 -0500

    Updated Jon and Noah

--- JonAndNoah.java
+++ JonAndNoah.java
@@ -1,33 +1,95 @@
+import java.io.*;
+import java.nio.*;
+import java.util.*;
 
 /**
- * Write a description of class JonAndNoah here.
- * Wuzzup
- * @author (your name)
- * @version (a version number or a date)
+ * JonAndNoah.
+ * Parse a Java program into individual words.
+ *
+ * @author Jonathan Katz and Noah Simmons
+ * @version 1.0
  */
-public class JonAndNoah
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class JonAndNoah {
+    BufferedReader reader;
+    String filename;
+    String delimiters = " ";
 
     /**
-     * Constructor for objects of class JonAndNoah
+     * Constructor for objects of class JonAndNoah.
+     * @param filename Name of file to read. Current directory assumed.
      */
-    public JonAndNoah()
-    {
-        // initialise instance variables
-        x = 0;
+    public JonAndNoah(String filename) {
+        this.filename = filename;
     }
-
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Show the filename associated with this class.
+     * @return filename that was given to the constructor.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String getFilename() {
+        return filename;
+    }
+    /**
+     * Open the file associated with this class.
+     * If a file was already open, this would close it.
+     * @return true if the file could be opened.
+     */
+    public void openFile () throws FileNotFoundException {
+        reader = null;
+        File file = new File(filename);
+        FileReader fr = new FileReader(file);
+        reader = new BufferedReader(fr);
+    }
+    /**
+     * Demonstrates how to use this class.
+     * Parses this Java program.
+     * Assumes the program is in the current directory.
+     * @param No parameter is required.
+     */
+    public static void main (String[] args) {
+        JonAndNoah jp = new JonAndNoah("JonAndNoah.java");
+        try {
+            jp.openFile();
+            jp.printLineByLine();
+        } catch (FileNotFoundException e) {
+            System.err.println("ERROR: cannot open "+jp.getFilename());
+        } catch (IOException e) {
+            System.err.println("ERROR: while reading "+jp.getFilename());
+        }
+    }   
+    /**
+     * Take input from the reader.
+     * Break the input into lines.
+     * On each line, call printWordByWord().
+     * Do nothing unless openFile() has run successfully.
+     */
+    public void printLineByLine() throws IOException {
+        String line;
+        if (reader!=null) {
+            do {  
+                line=reader.readLine();
+                if (line != null) {
+                    printWordByWord(line);
+                }
+            } while (line != null);
+        }
+    }
+    /**
+     * Break the given string into words.
+     * Use the delimiter that belongs to this.
+     * On each word, call printout().
+     */
+    public void printWordByWord (String oneline) {
+        String word;
+        StringTokenizer splitter = new StringTokenizer (oneline,delimiters);
+        while (splitter.hasMoreTokens()) {
+            word = splitter.nextToken();
+            printout(word);
+        }
+    }
+    /**
+     * Send the given string to the console.
+     */
+    public void printout (String s) {
+        System.out.println(s);
     }
 }
